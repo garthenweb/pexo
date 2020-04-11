94cb87c006cca54408ecfc7eda7ba7d70c247454
Jannick Garthen <jannick.garthen@gmail.com>
2020-04-11 11:57:53 +0200

    feat(cli): generate manifest for client bundle to be injected into the server

---
+++ src/cli/parcel-reporter-poxi-manifest/index.js
@@ -0,0 +1,37 @@
+const { Reporter } = require("@parcel/plugin");
+const path = require("path");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+
+exports.default = new Reporter({
+  async report({ event, options }) {
+    if (event.type !== "buildSuccess") {
+      return;
+    }
+    const { projectRoot } = options;
+
+    const manifest = {};
+    for (let bundle of event.bundleGraph.getBundles()) {
+      const mainBundleName = path.relative(
+        projectRoot,
+        bundle.getMainEntry().filePath
+      );
+      manifest[mainBundleName] = manifest[mainBundleName] || {
+        js: [],
+        css: [],
+      };
+      if (bundle.type === "js" || bundle.type === "css") {
+        manifest[mainBundleName][bundle.type].push(
+          path.relative(projectRoot, bundle.filePath)
+        );
+      }
+    }
+
+    options.outputFS.writeFile(
+      path.join(options.projectRoot, "dist", "manifest.json"),
+      JSON.stringify(manifest, null, "  ")
+    );
+  },
+});
