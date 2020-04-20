c831f89fc6d7877f7b4b1d319108eb57435e1a9c
Jannick Garthen <jannick.garthen@gmail.com>
2020-04-21 00:13:58 +0200

    fix(client): log in correct logger levels

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,8 +1,8 @@
 export const createDefaultLogger = (): Logger => ({
   info: (msg) => console.info(`[${new Date().toISOString()}] [PeXo INFO]`, msg),
-  warn: (msg) => console.info(`[${new Date().toISOString()}] [PeXo WARN]`, msg),
+  warn: (msg) => console.warn(`[${new Date().toISOString()}] [PeXo WARN]`, msg),
   error: (msg) =>
-    console.info(`[${new Date().toISOString()}] [PeXo WARN]`, msg),
+    console.error(`[${new Date().toISOString()}] [PeXo WARN]`, msg),
 });
 
 export interface Logger {
