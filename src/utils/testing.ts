bd275ef10f7806203c6e80b78912bad2231c06b4
Jannick Garthen <jannick.garthen@gmail.com>
2020-04-23 21:42:55 +0200

    fix(client): exclude react-dom/test-utils from production build

--- src/utils/testing.ts
+++ src/utils/testing.ts
@@ -1,11 +1,10 @@
-import { act } from "react-dom/test-utils";
-
 /**
  * Used to fix errors in react when testing
  * @caution In a perfect world this should never be used production code but only in tests. Always try to fix it in the tests first!
  */
 export const fireAsAct = (cb: () => void) => {
   if (process.env.NODE_ENV === "test") {
+    const { act } = require("react-dom/test-utils");
     act(() => cb());
   } else {
     cb();
